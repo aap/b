0c67f03467d5fd1f3a2bdd7a688fb5171c1f6b10
aap <aap@papnet.eu>
2021-06-24 14:37:36 +0200

    implemented argc,argv on amd64

--- amd64/brt1.s
+++ amd64/brt1.s
@@ -9,10 +9,51 @@ stack:	.=.+8*stacksz
 
 .globl	main, fetch
 main:
-	call	startchain
 	lea	stack(%rip),sp
 	mov	sp,dp
 	lea	init(%rip),pc
+
+	mov	%rdi,%r11	# argc
+	mov	%rsi,%r12	# argv
+	# B argc at 16(sp)
+	movq	%r11,16(sp)
+	# argv, have to make a B vector out of this, urgh
+	lea	(,%r11,8),%rax
+	sub	%rax,%rsp	# make space for argv array
+	# B argv at 24(sp)
+	mov	%rsp,%rbx
+	shr	$3,%rbx
+	mov	%rbx,24(sp)
+	# copy arguments to stack with word alignment
+	mov	%rsp,%rbx
+1:
+	test	%r11,%r11
+	jz	1f		# end of array
+	# count string
+	mov	$-7,%rcx
+	mov	(%r12),%rdi	# argv[i]
+	xor	%al,%al
+	repne scasb		# find end
+	# rcx is -(len+7) (including NUL) now
+	neg	%rcx
+	and	$-8,%rcx	# aligned to qwords
+	sub	%rcx,%rsp	# make space for it
+	# copy string
+	shr	$3,%rcx
+	mov	(%r12),%rsi
+	add	$8,%r12
+	mov	%rsp,%rdi
+	rep movsq
+	# store pointer in argv array
+	mov	%rsp,%rax
+	shr	$3,%rax
+	mov	%rax,(%rbx)
+	add	$8,%rbx
+	dec	%r11
+	jmp	1b
+1:
+
+	call	startchain
 fetch:
 	mov	(pc),%r10
 	add	$8,pc
@@ -26,7 +67,6 @@ init:
 .globl chain
 chain:
 	pop	%rsi
-	cld
 1:	lodsq
 	test	%rax,%rax
 	jz	1f
